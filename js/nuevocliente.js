b98c76b9240c04d9b65fa3f03114e2ee56fdaa5e
Marc-07 <maanraca01@gmail.com>
2023-06-27 12:16:36 -0500

    Se importa la funcion mostrar alerta

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,3 +1,5 @@
+import {mostrarAlerta} from './funciones.js'
+
 (function(){
 
     const formulario = document.querySelector('#formulario');
@@ -23,7 +25,7 @@
 
         if(validar(cliente)){
             //Mostrar mensaje
-            console.log('Todos los campos son obligatorios');
+            mostrarAlerta('Todos los campos son obligatorios');
             return;
         }
 
